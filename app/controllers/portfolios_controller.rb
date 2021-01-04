4ef80ec24e33abdeeb3f1a12e17829488fe54e18
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2021-01-04 18:57:01 +0000

    Refactored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,6 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: %i[edit show update destroy]
+
   layout 'portfolio'
 
   def index
@@ -14,13 +16,9 @@ class PortfoliosController < ApplicationController
     3.times { @portfolio_item.technologies.build }
   end
 
-  def edit
-    @portfolio_item = Portfolio.find(params[:id])
-  end
+  def edit; end
 
-  def show
-    @portfolio_item = Portfolio.find(params[:id])
-  end
+  def show; end
 
   def create
     @portfolio_item = Portfolio.new(portfolio_params)
@@ -34,7 +32,6 @@ class PortfoliosController < ApplicationController
   end
 
   def update
-    @portfolio_item = Portfolio.find(params[:id])
     respond_to do |format|
       if @portfolio_item.update(portfolio_params)
         format.html { redirect_to portfolios_path, notice: 'Portfolio updated successfully.' }
@@ -45,8 +42,6 @@ class PortfoliosController < ApplicationController
   end
 
   def destroy
-    @portfolio_item = Portfolio.find(params[:id])
-
     @portfolio_item.destroy
     respond_to do |format|
       format.html { redirect_to portfolios_path, notice: 'Portfolio destroyed successfully' }
@@ -54,6 +49,10 @@ class PortfoliosController < ApplicationController
   end
 
   private
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  end
 
   def portfolio_params
     params.require(:portfolio).permit(:title,
