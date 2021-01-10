e15dff8c3805d989e3eaf89e1cadaa0a302ddc41
Banjoko Abdulazeez <seunbanjoko2005@yahoo.com>
2021-01-10 00:09:32 +0000

    Add copyright view helper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include DefaultPageContent
+
+  before_action :set_copyright
+
+  def set_copyright
+    @copyright = AbdulViewTool::Renderer.copyright 'Banjoko Azeez', 'All rights reserved'
+  end
+end
+
+module AbdulViewTool
+  class Renderer
+    def self.copyright(name, message)
+      "&copy; #{Time.now.year} | <b>#{name}</b> #{message}".html_safe
+    end
+  end
 end
